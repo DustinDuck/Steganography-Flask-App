bbcc21d90a928a7250fab849eb16de274f0a15f1
root <root@VM164.local>
2024-10-15 16:58:56 -0400

    add AES encryption

--- main.py
+++ main.py
@@ -3,28 +3,20 @@ from flask import Flask, render_template
 from stega.Text.text import text
 
 
-UPLOAD_IMAGE_FOLDER = 'modes/Image/static'
-IMAGE_CACHE_FOLDER = 'modes/Image/__pycache__'
-UPLOAD_TEXT_FOLDER = 'modes/Text/static'
-TEXT_CACHE_FOLDER = 'modes/Text/__pycache__'
-UPLOAD_AUDIO_FOLDER = 'modes/Audio/static'
-AUDIO_CACHE_FOLDER = 'modes/Audio/__pycache__'
-UPLOAD_VIDEO_FOLDER = 'modes/Video/static'
-VIDEO_CACHE_FOLDER = 'modes/Video/__pycache__'
+UPLOAD_IMAGE_FOLDER = 'stega/Image/static'
+IMAGE_CACHE_FOLDER = 'stega/Image/__pycache__'
+UPLOAD_TEXT_FOLDER = 'stega/Text/static'
+TEXT_CACHE_FOLDER = 'stega/Text/__pycache__'
 
 
 # ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-app = Flask(__name__, template_folder='./modes/Text/templates')
+app = Flask(__name__, template_folder='./stega/Text/templates')
 app.secret_key = "hello"
 app.config['UPLOAD_IMAGE_FOLDER'] = UPLOAD_IMAGE_FOLDER
 app.config['IMAGE_CACHE_FOLDER'] = IMAGE_CACHE_FOLDER
 app.config['UPLOAD_TEXT_FOLDER'] = UPLOAD_TEXT_FOLDER
 app.config['TEXT_CACHE_FOLDER'] = TEXT_CACHE_FOLDER
-app.config['UPLOAD_AUDIO_FOLDER'] = UPLOAD_AUDIO_FOLDER
-app.config['AUDIO_CACHE_FOLDER'] = AUDIO_CACHE_FOLDER
-app.config['UPLOAD_VIDEO_FOLDER'] = UPLOAD_VIDEO_FOLDER
-app.config['VIDEO_CACHE_FOLDER'] = VIDEO_CACHE_FOLDER
 app.register_blueprint(text, url_prefix="/text")
 
 @app.route("/")
--- stega/Text/text.py
+++ stega/Text/text.py
@@ -7,6 +7,12 @@ from datetime import timedelta
 # from flask_wtf import FlaskForm
 from werkzeug.utils import secure_filename
 
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import unpad
+
+
 text = Blueprint("stega", __name__, static_folder="static",
                  template_folder="templates")
 
@@ -32,29 +38,33 @@ def text_encode():
 def text_encode_result():
     if request.method == 'POST':
         message = request.form['message']
+        encryption_type = request.form['encryption_type']
         if 'text_file' in request.files:
             text_file = request.files['text_file']
-            message = text_file.read().decode('utf-8')
+            if text_file and text_file.filename != '':
+                message = text_file.read().decode('utf-8')
 
-        if 'file' not in request.files:
+        if 'image_file' not in request.files:
             flash('No image found')
-        file = request.files['image']
+        image_file = request.files['image_file']
 
-        if file.filename == '':
+        if image_file.filename == '':
             flash('No image selected')
 
-        if file:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(
+        if image_file:
+            print(message)
+            filename = secure_filename(image_file.filename)
+            image_file.save(os.path.join(
                 current_app.config['UPLOAD_TEXT_FOLDER'], filename))
             text_encryption = True
-            encrypt_text(os.path.join(
+            key = encrypt_text(os.path.join(
                 current_app.config['UPLOAD_TEXT_FOLDER'], filename), message)
+            print(key)
         else:
             text_encryption = False
         result = request.form
 
-        return render_template("encode-text-result.html", result=result, file=file, text_encryption=text_encryption, message=message)
+        return render_template("encode-text-result.html", result=result, image_file=image_file, text_encryption=text_encryption, message=message, key=key)
 
 
 @text.route("/decode")
@@ -65,37 +75,64 @@ def text_decode():
 @text.route("/decode-result", methods=['POST', 'GET'])
 def text_decode_result():
     if request.method == 'POST':
-        if 'file' not in request.files:
+        key = request.form['key']
+        if 'image_file' not in request.files:
             flash('No image found')
-        file = request.files['image']
-        if file.filename == '':
+        image_file = request.files['image_file']
+        if image_file.filename == '':
             flash('No image selected')
-        if file:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(
+        if image_file:
+            filename = secure_filename(image_file.filename)
+            image_file.save(os.path.join(
                 current_app.config['UPLOAD_TEXT_FOLDER'], filename))
             text_decryption = True
             message = decrypt_text(os.path.join(
-                current_app.config['UPLOAD_TEXT_FOLDER'], filename))
+                current_app.config['UPLOAD_TEXT_FOLDER'], filename), key)
+            message = unpad(message, AES.block_size).decode()
         else:
             text_decryption = False
         result = request.form
-        return render_template("decode-text-result.html", result=result, file=file, text_decryption=text_decryption, message=message)
-
-# Encryption function
+        return render_template("decode-text-result.html", result=result, image_file=image_file, text_decryption=text_decryption, message=message)
 
 
-def encrypt_text(image_1, message):
+
+def encrypt_text(image_1, message, encryption_type):
     im = Image.open(image_1)
+
+    key = get_random_bytes(16)
+    key_hex = key.hex()
+
+    if encryption_type == "AES":
+        cipher = AES.new(key, AES.MODE_ECB)
+    elif encryption_type == "3DES":
+        cipher = DES3.new(key, DES3.MODE_ECB)
+    else:
+        raise ValueError("Invalid encryption type")
+
+    padded_message = pad(message.encode(), AES.block_size)
+    encrypted_message = cipher.encrypt(padded_message)
+
+    message = encrypted_message.hex()
 
     im1 = stepic.encode(im, bytes(str(message), encoding='utf-8'))
     im1.save(os.path.join(
         current_app.config['UPLOAD_TEXT_FOLDER'], "encrypted_text_image.png"))
+    return key_hex
 
-# Decryption function
-
-
-def decrypt_text(image_1):
+def decrypt_text(image_1, key_hex, encryption_type):
+    print(key_hex)
     im2 = Image.open(image_1)
     stegoImage = stepic.decode(im2)
-    return stegoImage
+
+    if encryption_type == "AES":
+        cipher = AES.new(bytes.fromhex(key_hex), AES.MODE_ECB)
+    elif encryption_type == "3DES":
+        cipher = DES3.new(bytes.fromhex(key_hex), DES3.MODE_ECB)
+    else:
+        raise ValueError("Invalid encryption type")
+    print("cipher:", cipher)
+    # fetch the encrypted message from the stego image
+    encrypted_message = bytes.fromhex(stegoImage)
+    # decrypt the message
+    decrypted_message = cipher.decrypt(encrypted_message)
+    return decrypted_message
